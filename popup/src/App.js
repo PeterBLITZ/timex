3b5b2a4b2f2f6e2bccefd3b3debc17e1c5d82b4d
Peter Blitz <artpit@gmail.com>
2019-07-01 20:15:57 +0300

    Render active hostname and timer [POC]
    Read data from the browser store.

--- popup/src/App.js
+++ popup/src/App.js
@@ -1,7 +1,42 @@
 import React from "react";
 
-function App() {
-  return <div>Timex</div>;
+const REFRESH_INTERVAL = 1000;
+
+class App extends React.Component {
+  state = {
+    activeHostname: "",
+    timestamp: null
+  };
+
+  componentDidMount() {
+    this.getActiveInterval = setInterval(this._getActive, REFRESH_INTERVAL);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.getActiveInterval);
+  }
+
+  _get = keys => global.browser.storage.local.get(keys);
+
+  _getActive = async () => {
+    try {
+      const { activeHostname } = await this._get("activeHostname");
+      const { timestamp } = await this._get("timestamp");
+      this.setState({ activeHostname, timestamp });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render() {
+    const time = this.state.timestamp ? Date.now() - this.state.timestamp : 0;
+    return (
+      <div>
+        <p>{this.state.activeHostname}</p>
+        <p>{time}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
